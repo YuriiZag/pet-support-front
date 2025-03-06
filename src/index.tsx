d118999b5ba502a8453ac85500a3d6f891eb1035
Yurii Zahrai <Zaghraj@i.ua>
2025-03-06 15:37:29 +0200

    Збережено зміни перед деплоєм

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Normalize } from "styled-normalize";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 import reportWebVitals from "./reportWebVitals";
@@ -14,10 +14,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/pet-support-front">
+      <HashRouter basename="/pet-support-front">
         <Normalize />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
